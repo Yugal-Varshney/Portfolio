bf372bcd4ab7ddd1f22ce5718e7aa724466fc016
Yugal Varshney <yugal.varshney0505@gmail.com>
2026-09-15 12:05:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ loadRepos();
 // Drop image files into /assets/drawings/ and list their filenames here.
 // Once populated, the placeholder message is replaced automatically.
 const DRAWINGS = [
-  'sketch_01.jpeg',
+  'sketch_01.png',
   'sketch_02.png',
   'sketch_03.jpg',
   'sketch_04.jpg',
